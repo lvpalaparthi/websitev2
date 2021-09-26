59708ed0f9a6a9a97d7c9c0244cdd0d9e8dac409
Lakshmi Palaparthi <lpalaparthi@Lakshmis-MacBook-Pro-3.local>
2021-09-26 18:05:37 -0400

    fixed media queries

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Portfolio from "./Portfolio";
 import Dance from "./Dance";
 import Contact from "./Contact";
 import Footer from "./Footer";
-import Icons from "./Icons";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Nav />
-      <Icons />
       <div class="App">
         <Switch>
           <Route path="/" exact component={Home} />
@@ -29,31 +27,31 @@ const Home = () => (
   <div>
     <link rel="stylesheet" type="text/css" href="/css/style.css" />
     <link rel="stylesheet" type="text/css" href="/css/queries.css" />
-    <body>
-      <header>
-        <div class="background-cover-box">
-          <h1 id="my_name">Lakshmi Palaparthi</h1>
-          <h3 id="description">Engineer.Choreographer.</h3>
-        </div>
-      </header>
+    <header>
+      <div class="background-cover-box">
+        <h1 id="my_name">
+          Lakshmi Palaparthi
+          <br /> Engineer.Choreographer.
+        </h1>
+        {/* <h1 id="my_name">Lakshmi Palaparthi</h1>
+        <h3 id="description">Engineer.Choreographer.</h3> */}
+      </div>
+    </header>
 
-      <section class="section-about-me">
-        <div class="row">
-          <div class="me-post">
-            <img id="me-img" src="/images/about_me.jpg" alt="profile" />
-            <p id="section-about-me">
-              Welcome! My name is Lakshmi Palaparthi, I am a software engineer
-              and an emerging choreographer currently based in the Philadelphia,
-              PA area. Be sure to click on the sidebar tabs to get a glimpse
-              into my story and check for any updates.
-            </p>
-            <a target="_blank" href="/resume/Lakshmi_Palaparthi_Resume.pdf">
-              <input id="resume-btn" type="button" value="View Resume" />
-            </a>
-          </div>
-        </div>
-      </section>
-    </body>
+    <section class="section-about-me">
+      <div class="me-post">
+        <img id="me-img" src="/images/about_me.jpg" alt="profile" />
+        <p id="section-about-me">
+          Welcome! My name is Lakshmi Palaparthi, I am a software engineer and
+          an emerging choreographer currently based in the Philadelphia, PA
+          area. Be sure to click on the sidebar tabs to get a glimpse into my
+          story and check for any updates.
+        </p>
+        <a target="_blank" href="/resume/Lakshmi_Palaparthi_Resume.pdf">
+          <input id="resume-btn" type="button" value="View Resume" />
+        </a>
+      </div>
+    </section>
   </div>
 );
 
--- src/Footer.js
+++ src/Footer.js
@@ -1,10 +1,76 @@
 import "./App.css";
 import React from "react";
+import {
+  FaGithub,
+  FaFacebook,
+  FaInstagram,
+  FaTwitter,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa";
 
 function Footer() {
   return (
-    <div class="footer-section">
-      <p>Copyright &copy; 2021 by Lakshmi Palaparthi. All rights reserved.</p>
+    <div>
+      <ul class="social-links">
+        <li>
+          <a
+            href="https://www.facebook.com/lakshmi.palaparthi.7/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaFacebook class="fb-icon" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/laksh_mi21"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaTwitter class="twitter-icon" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/lakshmipalaparthi/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin class="li-icon" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.instagram.com/palaparthi_lakshmi/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaInstagram class="ig-icon" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/lvpalaparthi"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FaGithub class="git-icon" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCowgcxgT0Q3YeOOijXWj4qA?view_as=subscriber"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaYoutube class="yt-icon" />
+          </a>
+        </li>
+      </ul>
+      <div class="footer-section">
+        <p>Copyright &copy; 2021 by Lakshmi Palaparthi. All rights reserved.</p>
+      </div>
     </div>
   );
 }
